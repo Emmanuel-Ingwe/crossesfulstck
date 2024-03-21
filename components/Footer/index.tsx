94f883cd159cca86c0564757ebe5f4d330925c4f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-03-21 12:33:43 +0100

    add: footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <footer className="flexStart footer">
+        <div className="flex flex-col gap-12 w-full">
+            <div className="flex items-start flex-col">
+                <Image src="/logo-purple.svg" width={115} height={38} alt="Flexibble"/>
+            </div>
+        </div>
+    </footer>
   )
 }
 
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { NavLinks } from '@/constants'
+import AuthProviders from "@/components/AuthProviders"
 
 type Props = {}
 
 const index = (props: Props) => {
+    const session = {}
+
+
   return (
     <nav className='flexBetween navbar'>
         <div className="flex-1 flexStart gap-10">
@@ -12,8 +17,25 @@ const index = (props: Props) => {
                 <Image src="/logo.svg" width={115} height={43} alt='Crosses' />
             </Link>
             <ul className="xl:flex hidden text-small gap-7">
-                 
+                 {NavLinks.map((link) => (
+                    <Link href={link.href} key={link.key}>
+                        {link.text}
+                    </Link>
+                 ))}
             </ul>
+        </div>
+
+        <div className="flexCenter gap-4">
+            {session ? (
+                <>
+                    UserPhoto
+                    <Link href="/create-project">
+                        Share Work
+                    </Link>
+                </>
+            ) : (
+                <AuthProviders />
+            )}
         </div>
     </nav>
   )
