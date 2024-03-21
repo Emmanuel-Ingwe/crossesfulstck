fb9155f40ee2b0a3b1be9437afa8ce30a68499aa
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-03-21 06:25:57 +0100

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Crosses</h1>
-      <h1>Crosses</h1>
-    </main>
+    <section className="flex-startt flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
+    </section>
   );
 }
