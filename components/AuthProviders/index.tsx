d4f18219c3575139c0269f33fcbdf8a326baa37d
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-03-25 05:12:16 +0100

    add: Provider(s)

--- components/AuthProviders/index.tsx
+++ components/AuthProviders/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client"
 
-type Props = {}
+import { Provider, useEffect, useState,  } from "react"
+import { getProviders, signIn } from "next-auth/react"
 
-const index = (props: Props) => {
-  return (
-    <div>index</div>
-  )
+type Providers = {
+  id: string
+  name: string
+  type: string
+  signinUrl: string
+  callbackUrl: string
+  signinUrlParams: Record<string, string> | null
+}
+
+const index = () => {
+  const [providers, setProviders] = useState<Providers | null>(null)
+
+  if(providers) {
+    return (
+      <div className="">
+        {Object.values(providers).map(
+          (provider: Provider, index) => (
+            <button className="" key={index}>{provider.id}</button>
+          )
+        )}
+      </div>
+    )
+  }
 }
 
 export default index
