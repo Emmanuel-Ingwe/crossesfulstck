cd3e9cbcbf551ef4709873f67bdffc7ca2a6a6fe
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-03-31 07:53:35 +0100

    :m-top

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -21,7 +21,7 @@ const FooterColumn = ({title, links}: ColumnProps) => (
 const index = () => {
 
   return (
-    <footer className="flexStart footer">
+    <footer className="flexStart footer lg:mt-28">
         <div className="flex flex-col gap-12 w-full">
             <div className="flex items-start flex-col">
                 <Image src="/logo-purple.svg" width={115} height={38} alt="Flexibble"/>
